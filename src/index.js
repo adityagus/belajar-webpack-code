2053e04b003da916cc09451c9e07de7cba967051
Aditya Gustian <adityagustian11@gmail.com>
2022-12-21 00:06:40 +0800

    menyelesaikan asset management

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
+// require ('./style.css');
+import * as bootstrap from 'bootstrap';
+import styles from './style.scss';
 import { run } from './app/app.js';
 import { AlertService } from './app/alert.service.js';
 import { CalculatorService } from './app/calculator.service.js';
 import { JokesService } from './app/jokes.service.js';
 
-alert("halo wpu")
 const alertService = new AlertService();
 const calculatorService = new CalculatorService();
 const jokesService = new JokesService();
 run(alertService, calculatorService, jokesService);
-
-
-
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,45 @@
+const MiniExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'output/admin'),
-    filename: 'bundle.js',
-    
-    
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: ('./src/template.html')
+    }),
+],
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+    ],
   },
-  watch: true,
-  devtool: false
-}
+  
+};
